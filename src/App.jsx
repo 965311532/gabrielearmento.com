3d99a9fd79bf323cd2e361cb4a6a42f25c4e9273
Gabriele Armento <gabriele.armento@gmail.com>
2023-02-05 23:04:35 +0100

    Make structure fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
       </Card>
       <Card
         sizeClass="h-full min-h-48"
-        spanClass="col-span-5"
+        spanClass="md:col-span-5 col-span-6"
         bgClass="bg-black"
       >
         <h1 className="text-2xl text-white">
           22 years old full stack software developer and aspiring entrepreneur
         </h1>
       </Card>
-      <div className="col-span-1 w-full grid grid-cols-1 gap-3">
+      <div className="col-span-6 md:col-span-1 w-full grid grid-cols-2 md:grid-cols-1 gap-3">
         <Card sizeClass="w-full" bgClass="bg-black">
           <div className="p-1">
             <img src="/public/linkedin-logo.svg" alt="linkedin logo" />
@@ -29,13 +29,25 @@ function App() {
           </div>
         </Card>
       </div>
-      <Card sizeClass="h-72" spanClass="col-span-3" bgClass="bg-black">
+      <Card
+        sizeClass="h-72"
+        spanClass="col-span-6 md:col-span-3"
+        bgClass="bg-black"
+      >
         <h1 className="text-2xl text-white">maps</h1>
       </Card>
-      <Card spanClass="col-span-3" bgClass="bg-black">
+      <Card
+        sizeClass="h-72"
+        spanClass="col-span-6 md:col-span-3"
+        bgClass="bg-black"
+      >
         <h1 className="text-2xl text-white">spoti</h1>
       </Card>
-      <Card spanClass="col-span-4" bgClass="bg-black">
+      <Card
+        sizeClass="h-72"
+        spanClass="col-span-6 md:col-span-4"
+        bgClass="bg-black"
+      >
         <h1 className="text-2xl text-white">About me</h1>
         <span className="text-white">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
@@ -44,7 +56,11 @@ function App() {
           quibusdam suscipit fuga!
         </span>
       </Card>
-      <Card spanClass="col-span-2" bgClass="bg-black">
+      <Card
+        sizeClass="h-72"
+        spanClass="col-span-6 md:col-span-2"
+        bgClass="bg-black"
+      >
         <h1 className="text-2xl text-white">stack</h1>
       </Card>
       <Card sizeClass="h-48" spanClass="col-span-6" bgClass="bg-black">
