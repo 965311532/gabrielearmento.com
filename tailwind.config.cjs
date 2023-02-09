8c23705c1870e0dd2cac9b68eb50ed479d8471f4
Gabriele Armento <gabriele.armento@gmail.com>
2023-02-09 21:57:36 +0100

    First iteration for location widget

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "ping-slow": "ping 2s cubic-bezier(0, 0, 0.2, 1) infinite",
+      },
+    },
   },
   plugins: [],
 };
