b39b383435223c52b9bf1c392a701fdc35f043af
Gabriele Armento <gabriele.armento@gmail.com>
2023-02-17 00:15:59 +0100

    Small changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       animation: {
         "ping-slow": "ping 2s cubic-bezier(0, 0, 0.2, 1) infinite",
-        "spin-very-slow": "spin 20s linear infinite",
+        "spin-very-slow": "spin 15s linear infinite",
       },
     },
   },
