4630b458f8d5a97e2f845cb73f1fc43c5cbd9646
Gabriele Armento <gabriele.armento@gmail.com>
2023-02-23 19:52:51 +0100

    UI small fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ spanClass, bgClass, sizeClass, children }) {
   // This is needed because Tailwind can't parse dynamic classes
   return (
     <div
-      className={`${spanClass} p-4 ring-1 ring-zinc-800/70 shadow-lg rounded-[20px] flex flex-col justify-center items-center ${bgClass} ${sizeClass}`}
+      className={`${spanClass} shadow-[inset_0_0_500px_-10px_rgba(255,255,255,0.1)] p-4 ring-1 ring-zinc-800/70 rounded-xl md:rounded-[20px] flex flex-col justify-center items-center ${bgClass} ${sizeClass}`}
     >
       {children}
     </div>
--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -1,9 +1,12 @@
 import clsx from "clsx";
+import regionToCountryCode from "../../lib/countryCodes.js";
 
 export default function Flag({ country, className, ...props }) {
+  var countryCode = regionToCountryCode[country];
+  if (!countryCode) return null;
   return (
     <img
-      src={`https://countryflagsapi.com/svg/${country}`}
+      src={`https://flagcdn.com/96x72/${countryCode}.png`}
       crossOrigin="anonymous"
       alt="country flag"
       className={clsx("inline-block h-4 w-auto", className)}
